d1bad51f8bae9386e97fa6891bbfae721f5ea511
Nafis Faiyaz <56973454+nafis420@users.noreply.github.com>
2022-11-03 01:10:29 -0400

    google Authentication successful

---
+++ client/src/App.js
@@ -0,0 +1,63 @@
+import logo from './logo.svg';
+// import React from 'react';
+import { useEffect,useState } from 'react';
+import jwt_decode from "jwt-decode";
+import './App.css';
+
+
+function App() {
+  const [user,setUser]=useState({}); 
+
+  function handleCallbackResponse(response){
+      console.log("Encoded JWT ID token: " + response.credential );
+      var userObject= jwt_decode(response.credential);
+      console.log(userObject);
+      setUser(userObject);
+      document.getElementById("signInDiv").hidden=true;
+  }
+
+  function handleSignOut(event){
+    setUser({});
+    document.getElementById("signInDiv").hidden=false;
+  }
+
+  useEffect(()=> {    
+     /*global google*/     
+    google.accounts.id.initialize({
+        client_id:"533084164543-b4o1rrbjehs2p0a55m7e7jkhl171tcrq.apps.googleusercontent.com",
+        callback: handleCallbackResponse
+  },[]);
+
+    google.accounts.id.renderButton(
+    document.getElementById("signInDiv"),
+    {theme: "outline",size:"large"}
+  );
+
+  google.accounts.id.prompt(); //popup prompt
+
+  }, []);
+
+  //if no user show sign in button
+
+  // is we have a user show the log out button
+  return (
+    
+    
+    <div className="App">
+      <div id="signInDiv"> </div>
+      {Object.keys(user).length !==0 &&
+         <button onClick={(e)=>handleSignOut(e)}>Sign Out </button>
+      }
+     
+      {user &&
+        <div>
+          <img src={user.picture} alt=" "></img>
+          <h3>{user.name}</h3>
+        </div>
+      }
+
+    </div>
+  );
+}
+
+export default App;
