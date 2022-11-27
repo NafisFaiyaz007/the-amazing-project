7f7018d96ed9845dab0e5e2ee3bdc9bc1697aa64
Nafis Faiyaz <56973454+NafisFaiyaz007@users.noreply.github.com>
2022-11-27 12:05:33 -0400

    week 4

--- src/components/FileViewer.js
+++ src/components/FileViewer.js
@@ -1,29 +1,29 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import FileViewer from "react-file-viewer";
+// // import React from "react";
+// // import ReactDOM from "react-dom";
+// // import FileViewer from "react-file-viewer";
 
-// import "./DragDrop.css";
+// // import "./DragDrop.css";
 
-// const file = "../CSE 299 project description.pdf";
-// const type = "pdf";
+// // const file = "../CSE 299 project description.pdf";
+// // const type = "pdf";
 
-// const onError = (e) => {
-//   console.log(e, "error in file-viewer");
-// };
+// // const onError = (e) => {
+// //   console.log(e, "error in file-viewer");
+// // };
 
-// export default function Viewer() {
-//   return (
-//     <div className="App">
-//       <h1>React File Viewer Demo</h1>
-//       <h2>Displaying file with extension {type}</h2>
-//       <FileViewer fileType={type} filePath={file} onError={onError} />
-//     </div>
-//   );
-// }
+// // export default function Viewer() {
+// //   return (
+// //     <div className="App">
+// //       <h1>React File Viewer Demo</h1>
+// //       <h2>Displaying file with extension {type}</h2>
+// //       <FileViewer fileType={type} filePath={file} onError={onError} />
+// //     </div>
+// //   );
+// // }
 
-// const rootElement = document.getElementById("fileviewer");
-// // eslint-disable-next-line no-undef
-// ReactDOM.render(<Viewer />, root);
+// // const rootElement = document.getElementById("fileviewer");
+// // // eslint-disable-next-line no-undef
+// // ReactDOM.render(<Viewer />, root);
 
 import React, { Component } from 'react';
 import logger from 'react-logger-lib';
@@ -32,7 +32,7 @@ import { CustomErrorComponent } from 'custom-error';
 
 
 const file = "../test.docx"
-const type = ['docx','pdf']
+const type = 'docx'
 
 export default class Viewer extends Component {
   render() {
@@ -49,3 +49,31 @@ export default class Viewer extends Component {
     logger.logError(e, 'error in file-viewer');
   }
 }
+// import "./styles.css";
+// import DocViewer, { DocViewerRenderers } from "react-doc-viewer";
+
+// export default function Viewer() {
+//   const docs = [
+    
+//     { uri: require("../CSE 299 project description.pdf") },
+   
+//   ];
+
+//   return (
+//     <div className="DragDrop">
+//       <h1>Sample react-doc-viewer</h1>
+//       <DocViewer
+//         pluginRenderers={DocViewerRenderers}
+//         documents={docs}
+//         config={{
+//           header: {
+//             disableHeader: false,
+//             disableFileName: false,
+//             retainURLParams: false
+//           }
+//         }}
+//         style={{ height: 500 }}
+//       />
+//     </div>
+//   );
+// }
--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 // import { UserAuth } from '../context/AuthContext';
 // import { Link } from "react-router-dom";
-import App from "../context/Drive";
+// import App from "../context/Drive";
 import Navbar from '../components/Navbar';
 import DragDrop from '../components/DragDrop';
+// import App from '../components/FileViewer';
 import Viewer from '../components/FileViewer';
 // import encrypt  from "../enc_dec";
 
@@ -27,12 +28,13 @@ const Account = () => {
         <div className="grid grid-cols-2 gap-4 ">
           <h1 className="p text-center text-l md:text-l lg:text-2xl text-white p-5">
             Import from drive
-            <App />
+            {/* <App /> */}
             <DragDrop/>
           </h1>
 
           <div className="text-center text-l md:text-l lg:text-2xl text-white p-5 float-right">
             <h1>view file</h1>
+            {/* <App/> */}
             <Viewer/>
           </div>
         </div>
