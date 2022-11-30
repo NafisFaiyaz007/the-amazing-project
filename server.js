17872c83f8099f1a7ef9f4e564aba1112286daaf
Ayman Ibne Hakim <aymanibne@hotmail.com>
2022-12-01 03:52:07 +0600

    Encryption_Decryption w/o View

---
+++ server.js
@@ -0,0 +1,145 @@
+var express = require('express');
+var app = express();
+var multer = require('multer')
+var cors = require('cors');
+var fs = require('fs');
+
+const { Console } = require('console');
+const crypto = require('crypto');
+const bodyParser = require('body-parser');
+app.use('/upload', express.static(__dirname + 'upload'));
+app.use(cors({
+  credentials: true,
+}));
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+    cb(null, 'upload')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' +file.originalname )
+  }
+})
+var upload = multer({ storage: storage }).single('file')
+const algorithm = 'aes-256-ctr';
+let key = 'MySecretKey';
+key = crypto.createHash('sha256').update(String(key)).digest('base64').substring(0, 32);
+function Encrypt  (buffer)  {
+// function EncryptFile (file) {
+  
+
+
+//Encryption
+    //Create inistialization vector
+    const iv = crypto.randomBytes(16);
+
+    //Create a new cypher using the algorithm , key, iv
+    const cipher = crypto.createCipheriv(algorithm, key, iv);
+    //Create new encrypted buffer
+    const result = Buffer.concat([iv, cipher.update(buffer), cipher.final()]);
+    console.log(result)
+    return result;
+}
+const decrypt = (encrypted) => {
+
+  //Get the IV : the first 16 bytes
+  const iv = encrypted.slice(0, 16);
+
+  //Get the rest
+  encrypted = encrypted.slice(16);
+
+  //Create decipher
+  const decipher = crypto.createDecipheriv(algorithm, key, iv);
+
+  //Decrypt it
+  const result = Buffer.concat([decipher.update(encrypted), decipher.final()]);
+  console.log("decrypted: ",result)
+  return result;
+}
+app.post('/upload',(req, res) => {
+    upload(req, res, function (err) {
+           if (err instanceof multer.MulterError) {
+               return res.status(500).json(err)
+           } else if (err) {
+               return res.status(500).json(err)
+           }
+          //  console.log(req.file.stream)
+    //  return res.status(200).send(req.file)
+      console.log(req.body)
+      
+      fs.readFile(`./upload/${req.file.filename}`, (err, file) => {
+        if (err)
+            return console.error(err.message);
+    
+      //  console.log(`Curent file data: ${file}`);
+    
+        //Encrypt the file
+        const encryptedFile = Encrypt(file);
+    
+        // Flow the encrypted file data to the new file
+        fs.writeFile(req.file.originalname, encryptedFile, (err, file) => {
+            if (err)
+                return console.error(err.message);
+            if (file) {
+                console.log('File Encrypted Successfully');
+            
+            }
+        })
+        fs.unlink(`./upload/${req.file.filename}`, (err => {
+          if (err) console.log(err);
+          else {
+            console.log(`\nDeleted file: ${req.file.filename}`);
+
+          }
+        }))
+        
+    })
+
+    // },
+    //   console.log(req.file))
+    // }, (req, res) => {
+
+    //   console.log(req.file);
+    //   console.log("file upload: ", req.file.originalname);
+
+      //saveEncryptedFile(req.file.buffer, path.join("./uploads", req.file.originalname), secret.key, secret.iv, req.file, req.user);
+      res.status(200).send(req.file)
+});
+      });
+
+
+app.post('/decrypt', (req, res) => {
+  upload(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+        return res.status(500).json(err)
+    } else if (err) {
+        return res.status(500).json(err)
+    }
+
+console.log(req.file)
+
+
+
+ fs.readFile(`./upload/${req.file.filename}`, (err, file) => {
+  if (err)
+      return console.error(err.message);
+
+  //Decrypt file
+  if (file) {
+      const decryptedFile = decrypt(file);
+      console.log(decryptedFile.toString());
+      fs.writeFile(req.file.originalname, decryptedFile, (err, file) => {
+          if (err)
+              return console.error(err.message);
+      })
+  }
+})
+
+res.status(200).send(req.file)
+});
+});
+
+app.listen(8080, function() {
+
+  console.log('App running on port 8080');
+
+});
--- src/components/DragDrop.jsx
+++ src/components/DragDrop.jsx
@@ -1,37 +1,92 @@
 import { useState } from "react";
 import { FileUploader } from "react-drag-drop-files";
-
+import {Buffer} from 'buffer';
+import axios from 'axios';
+// import PDFViewer from 'pdf-viewer-reactjs'
 import "./DragDrop.css";
+import Encrypt from "../context/encrypt";
 
-const fileTypes = ["JPG","DOCX","PDF"];
+const fileTypes = ["JPG","JPEG","DOCX","PDF", "txt"];
 
 export default function DragDrop() {
-  const [file, setFile] = useState(null);
+  const [file, setFile] = useState();
+  const [isFileSelected, setIsFileSelected] = useState(false)
+
   const handleChange = (file) => {
     setFile(file);
+    setIsFileSelected(true)
+    console.log(file.name)
+
+    // setFile = Encrypt(file);
+    // console.log("File Encrypted")
   };
-  return (
-    <div className="DragDrop text-center text-l md:text-l lg:text-2xl text-white p-auto">
+  
+  const handleEncryption = (file) => {
+    // if (!file){
+    //   return console.log("File not selected")
+    // }
+    // console.log( file)
+    // let reader = new FileReader();
+    
+    // reader.readAsText(file);
+    
+    // reader.onload = function() {
+    //   // var content = Buffer.from(reader.result)
+    //   console.log(reader.result);
+    //   window.Encrypt(reader.result)
+    // };
+    
+    // reader.onerror = function() {
+    //     console.log(reader.error);
+    //   };
+      
+    //   // console.log(content)
+    //   // Encrypt(content);
+    // console.log("File Encrypted")
+    const data = new FormData() 
+    data.append('file', file)
+    console.log("here")
+    axios.post("http://localhost:8080/upload", data, { // receive two parameter endpoint url ,form data 
+      })
+      .then(res => { // then print response status
+        console.log(res.statusText)
+      })
+      console.log("posted")
+  }
+  const handleDecryption = (file) => {
+    const data = new FormData() 
+    data.append('file', file)
+    axios.post("http://localhost:8080/decrypt", data, { // receive two parameter endpoint url ,form data 
+  })
+  .then(res => { // then print response status
+    console.log(res.statusText)
+  })
+  }
+      return (
+  <div className="DragDrop text-center text-l md:text-l lg:text-2xl text-white p-auto">
       {/* <h1>Hello To Drag & Drop Files</h1> */}
       <FileUploader handleChange={handleChange} name="file" types={fileTypes} />
       <p className="text-xs mt-5">
         {file ? `File name: ${file.name}` : "no files uploaded yet"}
       </p>
-      <p className="text-xs mt-5 md:px-auto">*Encrypt and store in drive</p>
+
+      <p className="text-xs mt-5">*Encrypt and store in drive</p>
       <p className="text-xs mt-1 mb-5">*Decrypt and view</p>
 
-      <button
-        className='bg-primary text-black rounded-full md:text-s lg:text-xl py-4 px-6 md:px-auto m-4 lg:py-2 lg:px-auto 
+      <button onClick={() => handleEncryption(file)}
+        className='bg-primary text-black rounded-full md:text-s lg:text-xl py-4 px-6 md:px-auto mb-4 mr-4 lg:py-2 lg:px-auto 
           font-bold uppercase cursor-pointer hover:opacity-75 duration-150" /> '
       >
-        {"Encrypt"}
-      </button>
-      <button
-        className='bg-secondary  rounded-full md:text-s lg:text-xl text-black py-4 px-6 m-4 md:px-auto lg:py-2 lg:px-auto
+        Encrypt
+      </button> 
+
+      <button onClick={() => handleDecryption(file)}
+        className='bg-secondary  rounded-full md:text-s lg:text-xl text-black py-4 px-6  md:px-auto lg:py-2 lg:px-auto
           font-bold uppercase cursor-pointer hover:opacity-75 duration-150" /> '
       >
-        {"Decrypt"}
+        Decrypt
       </button>
+      
     </div>
   );
 }
