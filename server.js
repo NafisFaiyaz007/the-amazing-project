b13ebc6ab619e845e55d893fd1b328db9bc56294
Ayman Ibne Hakim <aymanibne@hotmail.com>
2022-12-11 02:01:18 +0600

    Fixed File View and Encryption/Decryption

--- server.js
+++ server.js
@@ -5,13 +5,21 @@ var cors = require('cors');
 var fs = require('fs');
 const path = require('path');
 var drive = require('./src/googledrive/index')
+var filepath;
 
 const { Console } = require('console');
 const crypto = require('crypto');
 const bodyParser = require('body-parser');
+const { file } = require('googleapis/build/src/apis/file');
 app.use('/upload', express.static(__dirname + 'upload'));
+app.all('*', (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://localhost:3000");
+  next();
+});
 app.use(cors({
+  origin:'*', 
   credentials: true,
+  optionSuccessStatus:200
 }));
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -62,21 +70,20 @@ app.post('/upload',(req, res) => {
            } else if (err) {
                return res.status(500).json(err)
            }
-          //  console.log(req.file.stream)
-    //  return res.status(200).send(req.file)
+
       console.log(req.body)
       
       fs.readFile(`./upload/${req.file.filename}`, (err, file) => {
         if (err)
             return console.error(err.message);
     
-      //  console.log(`Curent file data: ${file}`);
+
     
         //Encrypt the file
         const encryptedFile = Encrypt(file);
     
         // Flow the encrypted file data to the new file
-        fs.writeFile(req.file.originalname, encryptedFile, (err, file) => {
+        fs.writeFile(`./Encrypted Files/${req.file.originalname}`, encryptedFile, (err, file) => {
             if (err)
                 return console.error(err.message);
             if (file) {
@@ -90,16 +97,11 @@ app.post('/upload',(req, res) => {
             console.log(`\nDeleted file: ${req.file.filename}`);
 
           }
-        }));drive.uploadFile('encryptedImage', `${req.file.originalname}`)
-         
+        }));
+
     })
 
-    // },
-    //   console.log(req.file))
-    // }, (req, res) => {
 
-    //   console.log(req.file);
-    //   console.log("file upload: ", req.file.originalname);
 
       //saveEncryptedFile(req.file.buffer, path.join("./uploads", req.file.originalname), secret.key, secret.iv, req.file, req.user);
       res.status(200).send(req.file)
@@ -117,7 +119,7 @@ app.post('/decrypt', (req, res) => {
 
 console.log(req.file)
 
- fs.readFile(`./upload/${req.file.filename}`, (err, file) => {
+ fs.readFile(`./Encrypted Files/${req.file.originalname}`, (err, file) => {
   if (err)
       return console.error(err.message);
 
@@ -125,17 +127,41 @@ console.log(req.file)
   if (file) {
       const decryptedFile = decrypt(file);
       console.log(decryptedFile.toString());
-      fs.writeFile(req.file.originalname, decryptedFile, (err, file) => {
-          if (err)
-              return console.error(err.message);
-      });drive.generatePublicUrl('10pVrSYLRhRwuKgPgLn3DvptCJaOXNXkS')
+      filepath = req.file.filename
+      fs.writeFile(`./Encrypted Files/${req.file.originalname}`, decryptedFile, (err, file) => {
+        if (err)
+        return console.error(err.message);
+        console.log(`${req.file.filename} has been decrypted`)
+      });
   }
 })
 
 res.status(200).send(req.file)
 });
 });
-//app.post(`/`)
+app.get('/update/:name', (req, res)=> {
+  const fileName = req.params.name;
+  fs.readFile(`./Encrypted Files/${fileName}`, (err, file) => {
+    if (err)
+        return console.error(err.message);
+        return res.send(file);
+      }
+         )
+})
+app.delete('/delete/:name', (req, res) => {
+  const fileName = req.params.name;
+  console.log(`the file is: ${req.params.name}`)
+  fs.unlink(`./Encrypted Files/${fileName}`, (err) => {
+    if (err) {console.log(err)
+    }
+
+   return res.status(200).send({
+      message: "File is deleted.",
+    });
+
+  });
+});
+
 app.listen(8080, function() {
 
   console.log('App running on port 8080');
--- src/components/DragDrop.jsx
+++ src/components/DragDrop.jsx
@@ -1,81 +1,150 @@
 import { useState } from "react";
 import { FileUploader } from "react-drag-drop-files";
-import {Buffer} from 'buffer';
 import axios from 'axios';
 // import PDFViewer from 'pdf-viewer-reactjs'
 import "./DragDrop.css";
-import Encrypt from "../context/encrypt";
-import { Document, Page } from 'react-pdf';
-
+// Import Worker
+import { Worker } from '@react-pdf-viewer/core';
+// Import the main Viewer component
+import { Viewer } from '@react-pdf-viewer/core';
+// Import the styles
+import '@react-pdf-viewer/core/lib/styles/index.css';
+// default layout plugin
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+// Import styles of default layout plugin
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import Modal from 'react-modal';
+import FileViewer from 'react-file-viewer';
 
 const fileTypes = ["JPG","JPEG","DOCX","PDF", "txt"];
 
 export default function DragDrop() {
   const [file, setFile] = useState();
-  const [isFileSelected, setIsFileSelected] = useState(false)
-  // const [numPages, setNumPages] = useState(null);
-  // const [pageNumber, setPageNumber] = useState(1);
-  // const [pdffile, setPdfFile] = useState();
+  const [openEx, setOpenEx] = useState(null);
+  const [pdfFile, setPdfFile] = useState();
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+  const [img, setImg] = useState(null);
+  const [doc, setDoc] = useState(null);
+  const [isDecrypted, setIsDecrypted] = useState(null);
 
-  
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = useState(false);
+  var filename = null;
+  const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+      width: '50%',
+      maxHeight: '95%'
+    },
+  };
+  function openModal(file) {
+    console.log("open modal")
+    itemOpenHandler(file)
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = '#f00';
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+    setFile('')
+    setPdfFile('')
+    setImg('')
+    setDoc('')
+    const data = new FormData() 
+    data.append('file', filename)
+    axios.delete(`http://localhost:8080/delete/${file.name}`, { // receive two parameter endpoint url ,form data 
+    // headers: {
+    //   "Access-Control-Allow-Origin": "*",
+    //   "credentials": "true",
+    // }
+  })
+  .then(res => { // then print response status
+    console.log(res.statusText)
+  })
+  }
   
   const handleChange = (file) => {
     setFile(file);
-    setIsFileSelected(true)
+    console.log(file)
     console.log(file.name)
+    filename  = file.name
+    console.log(`The name of the file is ${filename}.`)
+    console.log(file.name.split('.').pop() )
     // let reader = new FileReader();
-    // reader.readAsText(file);
+    // reader.readAsDataURL(file);
     // reader.onloadend=(e)=>{
     //   //setPdfError('');
     //   setPdfFile(e.target.result);}
-      
-      // setFile = Encrypt(file);
-      // console.log("File Encrypted")
+    
     };
     
     // function onDocumentLoadSuccess({ numPages }) {
     //   setNumPages(numPages);
     // }
     const handleEncryption = (file) => {
-    // if (!file){
-    //   return console.log("File not selected")
-    // }
-    // console.log( file)
-    // let reader = new FileReader();
-    
-    // reader.readAsText(file);
-    
-    // reader.onload = function() {
-    //   // var content = Buffer.from(reader.result)
-    //   console.log(reader.result);
-    //   window.Encrypt(reader.result)
-    // };
-    
-    // reader.onerror = function() {
-    //     console.log(reader.error);
-    //   };
-      
-    //   // console.log(content)
-    //   // Encrypt(content);
-    // console.log("File Encrypted")
+
     const data = new FormData() 
     data.append('file', file)
     console.log("here")
     axios.post("http://localhost:8080/upload", data, { // receive two parameter endpoint url ,form data 
-      })
+      headers: {
+      "Access-Control-Allow-Origin": "*",
+      "credentials": "true",
+    }    
+  })
       .then(res => { // then print response status
         console.log(res.statusText)
       })
+     
       console.log("posted")
   }
   const handleDecryption = (file) => {
     const data = new FormData() 
     data.append('file', file)
     axios.post("http://localhost:8080/decrypt", data, { // receive two parameter endpoint url ,form data 
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    }
   })
   .then(res => { // then print response status
     console.log(res.statusText)
   })
+  axios.get(`http://localhost:8080/update/${file.name}`, { // receive two parameter endpoint url ,form data 
+    })
+    .then(res => { // then print response status
+      console.log(res.statusText)
+      setFile(res.data)
+    })
+  itemOpenHandler(file)
+  }
+  const itemOpenHandler = (file) => {
+    setOpenEx(file.name.split('.')[1]);
+    if (file.name.split('.').pop() === 'pdf') {
+
+        let reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend=(e)=>{
+          //setPdfError('');
+          setPdfFile(e.target.result);}
+          // setFile('')
+          console.log("itemOpen hanler")
+    }
+    else if (file.name.split('.').pop() === 'jpg'){
+      setImg(file)
+    }
+    else {
+      setDoc(file)
+    }
+
   }
       return (
   <div className="DragDrop text-center text-l md:text-l lg:text-2xl text-white p-auto">
@@ -101,6 +170,62 @@ export default function DragDrop() {
       >
         Decrypt
       </button>
+      
+      
+         {/* style={{marginTop: '2rem', marginBottom: '5rem'}}> */}
+        <button onClick={() => openModal(file)} className='bg-secondary  rounded-full md:text-s lg:text-xl text-black py-4 px-6  md:px-auto lg:py-2 lg:px-auto
+          font-bold uppercase cursor-pointer hover:opacity-75 duration-150"/>'>View File</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
+        <button onClick={closeModal}>close</button>
+        <div>I am a modal</div>
+        <div style={{overflow: 'hidden', maxHeight: '20%'}}>
+
+        {pdfFile&&(
+          
+          <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.1.81/build/pdf.worker.min.js">
+    <Viewer fileUrl={pdfFile}
+    plugins={[defaultLayoutPluginInstance]}></Viewer>
+  </Worker>
+)}      
+
+</div>
+{img&&
+      <div >
+          {
+              img &&
+              <div>
+                  <img src={window.URL.createObjectURL(img)} alt=""/>
+              </div>
+          }
+      </div>}
+      {
+                doc &&
+                <FileViewer
+                    fileType={openEx}
+                    filePath={window.URL.createObjectURL(doc)}
+                />
+            }
+
+
+        <form>
+          <input />
+          <button>tab navigation</button>
+          <button>stays</button>
+          <button>inside</button>
+          <button>the modal</button>
+        </form>
+      </Modal>
+{/* render this if we have a pdf file */}
+
+
+
       {/* <div>
       <Document file={pdffile} onLoadSuccess={onDocumentLoadSuccess}>
         <Page pageNumber={pageNumber} />
